0ac5377af8a946f71a06ad272bb32165ee766350
Nikhil Bindal <nikhilbindal9654@gmail.com>
2024-04-12 11:31:33 +0530

    upgrate nextjs from 13 to 14

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Poppins } from "next/font/google";
 const Font = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["devanagari"],
+  subsets: ["latin"],
 });
 import Sidebar from "./Sidebar/sidebar";
 function Layout({ children }) {
